f02e8db23b304872e0af9a0662a36504cac57954
Manhmm204 <22003635.manh@student.iuh.edu.vn>
2024-08-15 00:17:19 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Do Duc Manh");
+        System.out.println("Welcome to th DHHTTT19A!");
+    }
+}
